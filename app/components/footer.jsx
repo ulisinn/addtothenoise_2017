0e3d3b09fa1aaa15d8009d723ffaa2da149cac4a
Ulrich Sinn <uli@addtothenoise.com>
2017-09-08 10:47:36 -0700

    footer updated

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,6 +5,7 @@ import '../styles/main.scss';
 
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import {Icon} from 'react-fa'
 
 import PropTypes from 'prop-types';
 import {actual} from 'actual';
@@ -13,23 +14,29 @@ export default class Footer extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
     // console.log('actual', actual.is('(max-width:30em)'));
     return (
       <div id="footer">
+        <a href={'https://www.instagram.com/laikasinn/'} target={'_blank'} style={{marginRight: '0.6em'}}>
+          <Icon name="instagram" style={{fontSize: '1.3em'}}/>
+        </a>
+        <a href={'https://www.linkedin.com/in/anabelsinn/'} target={'_blank'} style={{marginRight: '0.6em'}}>
+          <Icon name="linkedin" style={{fontSize: '1.1em'}}/>
+        </a>
         You can <a href="mailto:contact@addtothenoise.com?Subject=Hello">contact</a> addtothenoise here.
         {this.getLine2()}
       </div>  );
   }
-  
+
   showNavigation() {
     return <Navigation/>
   }
-  
+
   getLine2() {
     console.log('metaData width', this.props.metaData.width);
-  
+
     return (this.props.metaData.width > 650) ? <span> Also, Uli has an <Link to='/opinion'>OPINION</Link> (it was gracefully pointed out that it is not <em>his</em>).</span> : null;
   }
 }
