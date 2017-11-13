487d8bf428dee8e8f42ed0650f60e39bfa61ec36
Ulrich Sinn <uli@addtothenoise.com>
2017-11-13 13:32:15 -0800

    footer styling updated

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -20,29 +20,32 @@ export default class Footer extends Component {
     return (
       <div id="footer">
         <a href={'https://www.instagram.com/laikasinn/'} target={'_blank'} style={{marginRight: '0.6em'}}>
-          <Icon name="instagram" style={{fontSize: '1.3em'}}/>
+          <Icon name="instagram" className={'footerIcon'} style={{fontSize: '1.3em'}}/>
         </a>
         <a href={'https://www.linkedin.com/in/anabelsinn/'} target={'_blank'}
            style={{marginLeft: '0.6em', marginRight: '0.6em'}}>
-          <Icon name="venus" style={{color: '#bfbeb2', fontSize: '1.1em'}}/>
+          <Icon name="venus" className={'footerIcon'}/>
           <Icon name="caret-right"
-                style={{color: '#bfbeb2', marginLeft: '0.3em', marginRight: '0.3em', fontSize: '1.1em'}}/>
+                className={'footerIcon'}
+                style={{marginLeft: '0.3em', marginRight: '0.3em'}}/>
           <Icon name="linkedin" style={{fontSize: '1.1em'}}/>
         </a>
         <a href={'https://www.linkedin.com/in/ulrich-sinn/'} target={'_blank'}
            style={{marginLeft: '0.6em', marginRight: '0.6em'}}>
-          <Icon name="mars" style={{color: '#bfbeb2', fontSize: '1.1em'}}/>
+          <Icon name="mars" className={'footerIcon'}/>
           <Icon name="caret-right"
-                style={{color: '#bfbeb2', marginLeft: '0.3em', marginRight: '0.3em', fontSize: '1.1em'}}/>
-          <Icon name="linkedin" style={{fontSize: '1.1em'}}/>
+                className={'footerIcon'}
+                style={{marginLeft: '0.3em', marginRight: '0.3em'}}/>
+          <Icon name="linkedin" className={'footerIcon'}/>
         </a>
         <a href={'mailto:contact@addtothenoise.com?Subject=Hello'}
            style={{marginLeft: '0.6em', marginRight: '0.8em'}}>
-          <Icon name="envelope" style={{fontSize: '1.1em'}}/>
+          <Icon name="envelope"
+                className={'footerIcon'}/>
         </a>
         {/*You can <a href="mailto:contact@addtothenoise.com?Subject=Hello">contact</a> addtothenoise here.*/}
         {this.getLine2()}
-      </div>  );
+      </div>);
   }
 
   showNavigation() {
@@ -52,7 +55,8 @@ export default class Footer extends Component {
   getLine2() {
     console.log('metaData width', this.props.metaData.width);
 
-    return (this.props.metaData.width > 650) ? <span> Also, Uli has an <Link to='/opinion'>OPINION</Link> (it was gracefully pointed out that it is not <em>his</em>).</span> : null;
+    return (this.props.metaData.width > 650) ?
+      <span> Also, Uli has an <Link to='/opinion' className={'footerIcon'}>OPINION</Link> (it was gracefully pointed out that it is not <em>his</em>).</span> : null;
   }
 }
 
